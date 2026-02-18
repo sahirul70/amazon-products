fc3aacd7b2cb81ff8a4e3369c95d8de6b343f2e1
sahirul70 <sahirulislam.70@gmail.com>
2026-02-18 14:24:29 +0600

    this is commit

--- main.py
+++ main.py
@@ -1,40 +1,6 @@
-# import undetected_chromedriver as uc
-# driver = uc.Chrome(headless=True,use_subprocess=False)
-
-# from selenium.webdriver import Chrome
-
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
-# import json
-
-# driver = Chrome()
-# driver.get( 'https://www.amazon.com/s?k=laptop&crid=7RVWXM42GIWG&sprefix=laptop%2Caps%2C328&ref=nb_sb_noss_1' )
-
-# headline_element = driver.find_element(By.CSS_SELECTOR, "[data-cy=\"headline\"]")
-
-# title_element = headline_element.find_element(By.CSS_SELECTOR, "h1")
-# title = title_element.text
-
-# tagline_element = headline_element.find_element(By.CSS_SELECTOR, "h2")
-# tagline = tagline_element.text
-
-# description_element = headline_element.find_element(By.CSS_SELECTOR, "[data-cy=\"description\"]")
-# description = description_element.text
-# product = {
-#   "title": title,
-#   "tagline": tagline,
-#   "description": description
-# }
-
-# with open("product.json", "w") as json_file:
-#   json.dump(product, json_file, indent=4)
-  
-# # Close the browser and release its resources
-# driver.quit()
-
-
+import datetime
+from datetime import datetime
+import pandas as pd
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 import json
@@ -45,35 +11,67 @@ from selenium import webdriver
 # Create a Chrome web driver instance
 driver = webdriver.Chrome()
 time.sleep(5)
+driver.maximize_window()
+processed_data = []
 
 # Connect to the target page
-driver.get("https://www.amazon.com/s?k=laptop&crid=7RVWXM42GIWG&sprefix=laptop%2Caps%2C328&ref=nb_sb_noss_1")
+for i in range(1,20):
+  driver.get(f"https://www.amazon.com/s?k=electronic+tablets&page={i}&xpid=Kb09TIwp2AP1K&_encoding=UTF8&content-id=amzn1.sym.f0670b1b-e1fd-4c67-a2b1-b8a347243628&pd_rd_r=357b0bfd-1927-4dd1-953e-357d95f05740&pd_rd_w=rq7jY&pd_rd_wg=DujFk&qid=1771324497&ref=sr_pg_2")
 
-time.sleep(5)
-# Scraping logic
-headline_element = driver.find_element(By.XPATH, "(//div/a/h2/span){i}").text
+  time.sleep(5)
+  # Scraping logic
+  products = driver.find_elements(By.XPATH, "//div[@class='a-section']/div")
+  for product in products:
+      try:
+          title = product.find_element(By.XPATH, ".//h2/span").text
+          print(title)
+      except:
+          title = "N/A"
+        
+      try:
+          rating = product.find_element(By.XPATH, "(.//span[@class='a-size-small a-color-base'])").text
+          print(rating)
+      except:
+          rating = "N/A"
+        
+      try:
+          total_rating = product.find_element(By.XPATH, "(.//span[@class='a-size-mini puis-normal-weight-text s-underline-text'])").text
+          print(total_rating)
+      except:
+          total_rating = "N/A"
+        
+      try:
+          price = product.find_element(By.XPATH, "(.//span[@class='a-price-whole'])").text
+          print(price)
+      except:
+          price = "N/A"
 
-title_element = headline_element.find_element(By.CSS_SELECTOR, "h1")
-title = title_element.text
 
-tagline_element = headline_element.find_element(By.CSS_SELECTOR, "h2")
-tagline = tagline_element.text
+  # Populate a dictionary with the scraped data
+      product = {
+        "title": title,
+        "rating": rating,
+        "total_rating":total_rating,
+        "price": price
+      }
+      processed_data.append(product)
 
-description_element = headline_element.find_element(By.CSS_SELECTOR, "[data-cy=\"description\"]")
-description = description_element.text
+  # # Export the scraped data to JSON
+  with open("products.json", "w") as json_file:
+    json.dump(processed_data, json_file, indent=4)
+    
+  # Convert processed data to a Pandas DataFrame
+  df = pd.DataFrame(processed_data)
 
-# Populate a dictionary with the scraped data
-product = {
-  "title": title,
-  "tagline": tagline,
-  "description": description
-}
-
-# Export the scraped data to JSON
-with open("product.json", "w") as json_file:
-  json.dump(product, json_file, indent=4)
+    # Export DataFrame to CSV with current_datetime format
+  current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M")
+  csv_filename = f'products_{current_datetime}.csv'
+  df.to_csv(csv_filename, index=False, encoding='utf-8')
 
 # Close the browser and release its resources
 driver.quit() 
+
+
+
 
 
